fe5e9d23a17943492b51cdff61c257fda50da34f
Annisa Yansaf <yanann02@csgicorp.com>
2025-08-04 06:12:39 +0700

    fix parent table

--- app/Models/Parents.php
+++ app/Models/Parents.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Auth\Passwords\CanResetPassword as CanResetPasswordTrait;
 
 class Parents extends Model
